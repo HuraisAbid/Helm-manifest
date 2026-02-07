44460931e9affa39653e8ef299ada8e63406ad86
HuraisAbid <167809057+HuraisAbid@users.noreply.github.com>
2026-02-07 23:25:39 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,6 @@ app.get("/api/time", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend running on port 4 ${PORT}`);
+  console.log(`Backend running on port 5 ${PORT}`);
 });
 
